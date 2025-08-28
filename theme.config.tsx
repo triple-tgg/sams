e8d2f0be7172c5c1e6467f779e328fe7efd27918
triple-tgg <triplet.services.gg@gmail.com>
2025-08-28 14:25:01 +0700

    fix: build DashCodeLogo

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,8 @@
 
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
 import DashCodeLogo from '@/components/dascode-logo';
-const config: DocsThemeConfig = {
+
+const config = {
   logo: (
     <span className=" inline-flex gap-2.5 items-center">
       <DashCodeLogo className="  text-default-900 h-8 w-8 [&>path:nth-child(3)]:text-background [&>path:nth-child(2)]:text-background" />
