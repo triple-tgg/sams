e38fb517a819408232935b02445b34589329af6b
triple-tgg <triplet.services.gg@gmail.com>
2025-12-11 11:04:23 +0700

    fix

--- components/flight-timeline/index.ts
+++ components/flight-timeline/index.ts
@@ -2,7 +2,7 @@
 export { FlightTimelineWrapper } from './FlightTimelineWrapper';
 export { FlightProgram } from './FlightProgram';
 export { FlightChannel } from './FlightChannel';
-export { flightTimelineTheme } from './types';
+// export { flightTimelineTheme } from './types';
 export type { FlightEpgItem, AirlineChannel } from './types';
 export {
     transformFlightsToEpg,
